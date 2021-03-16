75d3d32e88a31d812542b615706ac559bfac7bd5
黃文彥 <huangwenyan@huangwenyandeMacBook-Air.local>
2021-03-16 11:34:57 +0800

    Finish 90%

---
+++ hack1/main.js
@@ -0,0 +1,57 @@
+// TODO:
+var comment_input = document.getElementById("comment-input");
+var cancel_button = document.getElementById("cancel-button");
+var comment_button = document.getElementById("comment-button");
+var div = document.getElementsByClassName("comment");
+var pdiv = document.getElementById("comment-group");
+var rec=""
+
+cancel_button.style.visibility="hidden"
+comment_button.style.visibility="hidden" 
+
+comment_input.oninput = handleInput;
+function handleInput(e) {
+    if(e.target.value.length > 0 && e.target.value.trim()!=""  ){
+        comment_button.disabled = false
+        comment_button.style.backgroundColor = "#065fd4"
+    }
+    else{
+        comment_button.disabled = true
+        comment_button.style.backgroundColor = "#cccccc"
+    }
+  }
+comment_input.addEventListener('focusin', (event) => {
+    cancel_button.style.visibility="visible"
+    comment_button.style.visibility="visible" 
+  });
+/*
+  comment_input.addEventListener('focusout', (event) => {
+    cancel_button.style.visibility="hidden"
+    comment_button.style.visibility="hidden" 
+  });  
+*/
+
+comment_input.addEventListener(
+    "change",
+    function(t) {        
+        rec = t.target.value
+        t.target.value=""
+        comment_button.style.backgroundColor = "#cccccc"        
+    }
+);
+cancel_button.addEventListener(
+    "click",
+    function() {        
+        cancel_button.style.visibility="hidden"
+        comment_button.style.visibility="hidden"           
+    }
+);
+comment_button.addEventListener(
+    "click",
+    function() {      
+        var clone = div[0].cloneNode(true);
+        clone.childNodes[3].childNodes[3].textContent = rec
+        console.log(clone.childNodes[3].childNodes[3].textContent)
+        pdiv.appendChild(clone);
+    }
+);
